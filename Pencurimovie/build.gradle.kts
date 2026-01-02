fda01ad85af93ee4ffd3c4f7be66076ccf456462
angahjee1994 <46360870+angahjee1994@users.noreply.github.com>
2026-01-02 10:44:48 +0800

    .

--- Pencurimovie/build.gradle.kts
+++ Pencurimovie/build.gradle.kts
@@ -6,7 +6,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     description = "Pencurimovie "
-    language    = "id"
+    language    = "ms"
     authors = listOf("Phisher98")
 
     /**
