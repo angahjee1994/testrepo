b5137edd833d854a060f08ce22daed5595d07ff5
angahjee1994 <46360870+angahjee1994@users.noreply.github.com>
2026-01-20 23:23:49 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.13.0")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
 
