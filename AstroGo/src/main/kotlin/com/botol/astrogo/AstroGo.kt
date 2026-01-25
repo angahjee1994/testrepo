9a6755631046bb47ca8a52f733c35aa5f5f29bce
angahjee1994 <46360870+angahjee1994@users.noreply.github.com>
2026-01-25 21:21:24 +0800

    Update AstroGo.kt

--- AstroGo/src/main/kotlin/com/botol/astrogo/AstroGo.kt
+++ AstroGo/src/main/kotlin/com/botol/astrogo/AstroGo.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 class AstroGo : MainAPI() {
     override var mainUrl = "https://astrogo.astro.com.my"
-    private val apiUrl = "https://sg-sg-sg.astro.com.my/ctap/r1.6.0"
+    private val apiUrl = "https://sg-sg-sg.astro.com.my:9443/ctap/r1.6.0"
     override var name = "AstroGo"
     override val hasMainPage = true
     override var lang = "ms"
