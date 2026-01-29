1b2c349be3313d21e6e9d0176be8f0e8656c6cf9
angahjee1994 <46360870+angahjee1994@users.noreply.github.com>
2026-01-29 09:16:21 +0800

    .

---
+++ AstroGoTEST/build.gradle.kts
@@ -0,0 +1,23 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    language = "ms"
+    // All of these properties are optional, you can safely remove them
+
+    description = "Astro Go TESTING BELOM SIAP SABAR"
+    authors = listOf("botol")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 3 // will be 3 if unspecified
+    tvTypes = listOf("LiveTV", "Movies", "TvSeries")
+
+    iconUrl = "https://raw.githubusercontent.com/angahjee1994/testrepo/refs/heads/main/AstroGo/logo.png"
+}
