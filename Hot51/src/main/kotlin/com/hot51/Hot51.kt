f0dbe165c5c01d2f91317c47ef60f0db5f742572
angahjee1994 <46360870+angahjee1994@users.noreply.github.com>
2026-02-04 07:25:20 +0800

    Update Hot51.kt

--- Hot51/src/main/kotlin/com/hot51/Hot51.kt
+++ Hot51/src/main/kotlin/com/hot51/Hot51.kt
@@ -40,7 +40,9 @@ class Hot51 : MainAPI() {
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val labelId = if (request.data.isNullOrBlank() || request.data == "popular") "1" else request.data
         val timestamp = System.currentTimeMillis() / 1000
-        val url = "$apiUrl/public/live/lrl?pageNum=$page&pageSize=50&labelId=$labelId&merchantId=$merchantId&area=MY&lang=ENU&t=$timestamp"
+        
+        val baseUrl = "$apiUrl/public/live/lrl?pageNum=$page&pageSize=50&merchantId=$merchantId&area=MY&lang=ENU&t=$timestamp"
+        val url = if (labelId == "1") baseUrl else "$baseUrl&labelId=$labelId"
         
         val response = app.get(url).parsedSafe<LiveCenterResponse>()
         val items = response?.records?.map { item ->
