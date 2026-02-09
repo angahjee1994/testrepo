f66033199b4d09304c0affa7021e0720791eb1f7
angahjee1994 <46360870+angahjee1994@users.noreply.github.com>
2026-02-09 19:39:19 +0800

    Update Moviebox.kt

--- Moviebox/src/main/kotlin/com/hexated/Moviebox.kt
+++ Moviebox/src/main/kotlin/com/hexated/Moviebox.kt
@@ -32,7 +32,7 @@ class Moviebox : MainAPI() {
         private val bannedLanguages = setOf(
             "English", "French", "Hindi", "Bengali", "Urdu", "Punjabi",
             "Tamil", "Telugu", "Malayalam", "Kannada", "Arabic",
-            "Tagalog", "Indonesian", "Russian", "Malay", "Gujarati", "Portuguese", "Kurdish"
+            "Tagalog", "Indonesian", "Russian", "Gujarati", "Portuguese", "Kurdish"
         )
     }
 
